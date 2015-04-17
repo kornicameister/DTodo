6db8922160f2a46507ac8cefdfa5d189ba6893ec
Tomasz Trębski <kornicameister@gmail.com>
2015-04-17 19:37:57 +0200

    - reworked tags to use passed user instance [from current request]

--- DTodo/templatetags/header_tags.py
+++ DTodo/templatetags/header_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django_tools.middlewares import ThreadLocal
 
 from DTodo.models import Todo
 
@@ -9,16 +8,14 @@ register = template.Library()
 
 @register.inclusion_tag(file_name='base/components/recent_todos.html',
                         name='recenttodos')
-def recent_todos_tag(**kwargs):
-    current_user = ThreadLocal.get_current_user()
-    is_authenticated = current_user.is_authenticated()
-    limit = kwargs.get('limit', 10)
+def most_completed_todos_tag(user, limit=10):
+    is_authenticated = user.is_authenticated()
     order = '-updated_at'
 
     if not is_authenticated:
         qs = Todo.objects.filter(visibility=Todo.public_visibility())
     else:
-        qs = Todo.objects.filter(owned_by_id=current_user)
+        qs = Todo.objects.filter(owned_by_id=user)
 
     return {
         'todos': qs.order_by(order)[:limit]
@@ -28,17 +25,14 @@ def recent_todos_tag(**kwargs):
 @register.inclusion_tag(
     file_name='base/components/most_completed_todos.html',
     name='mostcompletedtodos')
-def most_completed_todos_tag(**kwargs):
-    current_user = ThreadLocal.get_current_user()
-    is_authenticated = current_user.is_authenticated()
+def most_completed_todos_tag(user, limit=10):
+    is_authenticated = user.is_authenticated()
     order = '-updated_at'
-
-    limit = kwargs.get('limit', 10)
 
     if not is_authenticated:
         qs = Todo.objects.filter(visibility=Todo.public_visibility())
     else:
-        qs = Todo.objects.filter(owned_by_id=current_user)
+        qs = Todo.objects.filter(owned_by_id=user)
 
     qs = qs.order_by(order)
     qs = sorted(qs, key=lambda todo: todo.progress, reverse=True)
@@ -51,10 +45,8 @@ def most_completed_todos_tag(**kwargs):
     file_name='base/components/header-user.html',
     name='user_menu')
 def user_menu_tag(user):
-    # note that as example we pass user to the tag directly
-    current_user = ThreadLocal.get_current_user()
-    is_authenticated = current_user.is_authenticated()
+    is_authenticated = user.is_authenticated()
     return {
-        'username': None if not is_authenticated else current_user.username,
+        'username': None if not is_authenticated else user.username,
         'authenticated': is_authenticated
     }
